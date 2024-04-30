c4ac38611681c81e69d7b0c927eaeaf11885b5be
uvins <114795545+uvins@users.noreply.github.com>
2024-04-30 11:48:50 +0300

    Add files via upload

---
+++ Jalog.kt
@@ -0,0 +1,71 @@
+package org.example
+
+import java.text.SimpleDateFormat
+import java.util.*
+
+class Jalog {
+    private var ansiReset = "\u001B[0m"
+    private var ansiRed = "\u001B[31m"
+    private var ansiGreen = "\u001B[32m"
+    var ansiYellow = "\u001B[33m"
+    var ansiBlue = "\u001B[34m"
+
+    private var date = Date()
+    private var usaFormat = true
+
+    fun euroFormat(euroFormat: Boolean) {
+        usaFormat = !euroFormat
+    }
+
+    fun info(msg: String) {
+
+        val formater: SimpleDateFormat = if (usaFormat) {
+            SimpleDateFormat("MM.dd HH:mm:ss")
+        } else {
+            SimpleDateFormat("dd.MM HH:mm:ss")
+        }
+
+        val result = ansiGreen + "[INFO | ${formater.format(date)}] $msg" + ansiReset
+
+        println(result)
+    }
+
+    fun warn(msg: String) {
+
+        val formater: SimpleDateFormat = if (usaFormat) {
+            SimpleDateFormat("MM.dd HH:mm:ss")
+        } else {
+            SimpleDateFormat("dd.MM HH:mm:ss")
+        }
+
+        val result = ansiYellow + "[WARN | ${formater.format(date)}] $msg" + ansiYellow
+
+        println(result)
+    }
+
+    fun error(msg: String) {
+
+        val formater: SimpleDateFormat = if (usaFormat) {
+            SimpleDateFormat("MM.dd HH:mm:ss")
+        } else {
+            SimpleDateFormat("dd.MM HH:mm:ss")
+        }
+
+        val result = ansiRed + "[ERROR | ${formater.format(date)}] $msg" + ansiReset
+
+        println(result)
+    }
+
+    fun custom(option: String, msg: String) {
+
+        val formater: SimpleDateFormat = if (usaFormat) {
+            SimpleDateFormat("MM.dd HH:mm:ss")
+        } else {
+            SimpleDateFormat("dd.MM HH:mm:ss")
+        }
+
+        val result = ansiBlue + "[${option} | ${formater.format(date)}] $msg" + ansiReset
+
+        println(result)
+    }
+}
