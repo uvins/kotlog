b03095a0e1cc08cf46f872be1bfdcb059c0f949c
uvins <114795545+uvins@users.noreply.github.com>
2024-04-30 11:55:45 +0300

    Update and rename main.kt to docs.kt

--- docs.kt
+++ docs.kt
@@ -1,8 +1,9 @@
 fun main() {
   val log = Kotlog()
-
+  
+  // dd.MM or MM.dd format
   log.euroFormat(true)
-
+  
   log.info("Information Text...")
   log.warn("Warn Text...")
   log.error("Error Text...")
